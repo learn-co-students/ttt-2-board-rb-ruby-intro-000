03000e99d830329f81113ea18601a1020444de64
Clementieva Olga <fedotovaolea@gmail.com>
2016-11-03 19:47:41 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,14 +1,1 @@
-# Define the variable board below.
-row = ["","",""]
-
-# Position 1: Left
-row[0]
-
-# Position 2: Middle
-row[1]
-
-# Position 3: Right
-row[2]
-#Move X to the Position 2, Middle
-row[1]="X"
-row #=>["","X",""]
+board = Array.new(9," ")
