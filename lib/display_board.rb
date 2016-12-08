e2e09a027611ce33deb4f1d375d9b0680c36088d
Tom Nolan <tom.pat.nolan@gmail.com>
2016-12-08 15:02:33 +0000

    Done.

---
+++ lib/display_board.rb
@@ -0,0 +1,22 @@
+def display_board
+ msg="Welcome to Tic Tac Toe"
+ c1="   "
+ c2="   "
+ c3="   "
+ c4="   "
+ c5="   "
+ c6="   "
+ c7="   "
+ c8="   "
+ c9="   "
+ line1 = "#{c1}|#{c2}|#{c3}"
+ line2 = "#{c4}|#{c5}|#{c6}"
+ line3 = "#{c7}|#{c8}|#{c9}"
+ separator = "-----------"
+  puts msg
+  puts line1
+  puts separator
+  puts line2
+  puts separator
+  puts line3
+end
