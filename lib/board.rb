6e1cb9d6e7de127c68c26bce8abf50f88fd0602d
Jul Pro <progujul@gmail.com>
2017-03-02 16:07:33 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,64 +1,28 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 # Position 1: Left
-board[0]
+board[0] #=> "X"
 
 # Position 2: Middle
-board[0]
+board[1] #=> "O"
 
 # Position 3: Right
-board[0]
+board[2] #=> "X"
 
 # Position 4: Left
-board[1]
+board[3] #=> "X"
 
 # Position 5: Middle
-board[1]
+board[4] #=> "O"
 
 # Position 6: Right
-board[1]
+board[5] #=> "O"
 
 # Position 7: Left
-board[2]
+board[6] #=> "O"
 
 # Position 8: Middle
-board[2]
+board[7] #=> "X"
 
 # Position 9: Right
-board[2]
-
-# Move X to the Position 1: Left
-board[0] = "X"
-board #=> ["X", " ", " ", " ", " ", " ", " ", " ", " "]
-
-# Move O to the Position 5: Middle
-board[1] = "O"
-board #=> ["X", " ", " ", " ", "O", " ", " ", " ", " "]
-
-# Move X to the Position 3: Right
-board[0] = "X"
-board #=> ["X", " ", "X", " ", "O", " ", " ", " ", " "]
-
-# Move O to the Position 2: Middle
-board[1] = "O"
-board #=> ["X", "O", "X", " ", "O", " ", " ", " ", " "]
-
-# Move X to the Position 8: Middle
-board[2] = "X"
-board #=> ["X", "O", "X", " ", "O", " ", " ", "X", " "]
-
-# Move O to the Position 6: Right
-board[1] = "O"
-board #=> ["X", "O", "X", " ", "O", "O", " ", "X", " "]
-
-# Move X to the Position 4: Left
-board[1] = "X"
-board #=> ["X", "O", "X", "X", "O", "O", " ", "X", " "]
-
-# Move O to the Position 7: Left
-board[2] = "O"
-board #=> ["X", "O", "X", "X", "O", "O", "O", "X", " "]
-
-# Move X to the Position 9: Right
-board[2] = "X"
-board #=> ["X", "O", "X", "X", "O", "O", "O", "X", "X"]
+board[8] #=> "X"
