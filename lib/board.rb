be386c5c1dfdebba4fc78f6b9e01e2a9c02c0fc4
Glenn Holt <glennholt1@gmail.com>
2017-07-14 00:01:59 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,4 @@
 # Define the variable board below.
+
+board = [" "," "," "," "," "," "," "," "," "]
+#https://stackoverflow.com/questions/24432225/fill-array-with-element-n-times   explains other intricies of filling repeated values
