7baa8bebae0f1057d0af0b72fc97d3e32d939a79
Daniel Mack <dmmshadow@aol.com>
2017-10-11 18:51:21 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,12 @@
 # Define the variable board below.
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+
+puts def display_board
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+end
+
+display_board(board)
