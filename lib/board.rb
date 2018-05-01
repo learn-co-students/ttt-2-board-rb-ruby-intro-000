3414813b9b5eb686d8338442b23afbacf4841357
Alexandra Carroll <alexandrajc94@gmail.com>
2018-05-01 04:50:07 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,22 @@
-# Define the variable board below.
+my_array=["board"]
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+row = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+# Position 1: Left
+row[0]
+# Position 2: Middle
+row[1]
+# Position 3: Right
+row[2]
+# Position 4: Left
+row[3]
+# Position 5: Middle
+row[4]
+# Position 6: Right
+row[5]
+# Position 7: Left
+row[6]
+# Position 8: Middle
+row[7]
+# Position 9: Right
+row[8]
