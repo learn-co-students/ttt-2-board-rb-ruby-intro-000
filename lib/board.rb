199b0e03d1f908fa801ef36c88b6f18df6962d93
Juni T <jenni@rapini.org>
2018-07-03 22:01:35 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,19 @@
-# Define the variable board below.
+board = [" "," "," "," "," "," "," "," "," "]
+# Position 1: Top Left
+row[0]
+# Position 1: Top Middle
+row[1]
+# Position 1: Top Right
+row[2]
+# Position 1: Middle Left
+row[3]
+# Position 1: Middle  Middle
+row[4]
+# Position 1: Middle Right
+row[5]
+# Position 1: bottom left
+row[6]
+# Position 1: bottom Middle
+row[7]
+# Position 1: bottom right
+row[8]
