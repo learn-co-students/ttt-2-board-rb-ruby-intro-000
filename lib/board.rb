94e2e75d643ab749a4d665d9d62e925e98ac1126
Steven Williams <evilgeniusnyc@me.com>
2020-02-24 15:56:19 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,55 @@
-# Define the variable board below.
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+
+
+
+
+
+# `# Define the variable board below.
+# Board.rb = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+# # Position 1: Upper Left
+# board.rb [0]
+# # Position 2: Upper Middle
+# board.rb [1]
+# # Position 3: Upper Right
+# board.rb [2]
+# # Position 4: Middle Left
+# board.rb [3]
+# # Position 5: Middle Middle
+# board.rb [4]
+# # Position 6: Middle Right
+# board.rb [5]
+# # Position 7: Lower Left
+# board.rb [6]
+# #Position 8: Lower Middle
+# board.rb [7]
+# # Position 9: Lower Right
+# board.rb [8]
+# board.rb [0] = "X"
+# # Move X to Position 1 Upper Left
+# board.rb #=> ["X", " ", " ", " "," ", " ", " ", " ", " "]
+# # Move X to Position 2 Upper Middle
+# board.rb [1] = "X"
+# board.rb #=> ["X", "X", " ", " "," ", " ", " ", " ", " "]
+# # Move X to Position 3 Upper Right
+# board.rb [2] = "X"
+# board.rb #=> ["X", "X", "X", " "," ", " ", " ", " ", " "]
+# # Move X to Position 4 Middle Left
+# board.rb [3] = "O"
+# board.rb #=> ["X", "X", "X", "O"," ", " ", " ", " ", " "]
+# # Move X to Position 5 Middle Middle
+# board.rb [4] = "O"
+# board.rb #=> ["X", "X", "X", "O", "O", " ", " ", " ", " "]
+# # Move X to Position 6 Middle Right
+# board.rb [5] = "X"
+# board.rb #=> ["X", "X", "X", "O", "O", "X", " ", " ", " "]
+# # Move X to Position 2 Lower Left
+# board.rb [6] = "O"
+# board.rb #=> ["X", "X", "X", "O", "O", "X", "O", " ", " "]
+# # Move X to Position 2 Lower Middle
+# board.rb [7] = "O"
+# board #=> ["X", "X", "X", "O", "O", "X", "O", "O", " "]
+# # Move X to Position 2 Lower Right
+# board.rb [8] = "X"
+# board.rb #=> ["X", "X", "X", "O", "O", "X", "O", "O", "X"]
+# `
