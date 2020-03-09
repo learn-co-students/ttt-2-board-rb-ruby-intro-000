62b2c9738b46d3811424467aadd80695e0c155cc
secure-analyst-1223 <z.chium@gmail.com>
2020-03-09 15:55:33 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,21 @@
-# Define the variable board below.
+# array variables
+board = Array.new
+board = [" ", " ", " "," ", " ", " ", " ", " ", " "]
+# Position 1: Left
+board[0]
+# Position 2: Middle
+board[1]
+# Position 3: Right
+board[2]
+# Position 4: Middle Left
+board[3]
+# Position 5: Middle Center
+board[4]
+# Position 6: Middle Right
+board[5]
+# Position 7: Bottom Left
+board[6]
+# Position 8: Bottom Middle
+board[7]
+# Position 9: Bottom Right
+board[8]
