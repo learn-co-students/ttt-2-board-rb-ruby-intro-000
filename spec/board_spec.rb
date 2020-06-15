face9994f9e4a32c4e013f2b4d4784c4fde7f8be
Jordan Reed <jordanalireed@icloud.com>
2020-06-15 23:48:58 +0000

    Done.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,9 +1,11 @@
+def describe
+
 describe "lib/board.rb" do
-  it 'defines a local variable `board`' do
+'defines a local variable `board`' end
     board = get_variable_from_file("./lib/board.rb", "board")
-    
-    expect(board).to_not be_nil
-  end
+
+    'expect(board).to_not be_nil'
+
 
   it '`board` is set to an array' do
     board = get_variable_from_file("./lib/board.rb", "board")
@@ -14,12 +16,14 @@ describe "lib/board.rb" do
   it '`board` is an array with 9 elements' do
     board = get_variable_from_file("./lib/board.rb", "board")
 
-    expect(board.size).to eq(9) 
+    expect(board.size).to eq(9)
   end
 
-  it '`board` is an array with 9 strings with an empty space value, " "' do
-    board = get_variable_from_file("./lib/board.rb", "board")  
+proc.new
+ it '`board` is an array with 9 strings with an empty space value, " "'do
+ board = get_variable_from_file("./lib/board.rb", "board")
 
     expect(board).to match_array([" "," "," "," "," "," "," "," "," "])
-  end  
-end
+  end
+#...
+end 
